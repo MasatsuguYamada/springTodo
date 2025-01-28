7cff575e1a79968b1650f8d852b32545709d360e
Tsugu <masatsugu_yamada@mail.toyota.co.jp>
2025-01-28 11:52:58 +0900

    docker-compose準備

---
+++ init/create-table.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+awslocal dynamodb create-table --table-name LocalTodoTable\
+ --attribute-definitions AttributeName=PK,AttributeType=S\
+  --key-schema AttributeName=PK,KeyType=HASH\
+   --billing-mode PAY_PER_REQUEST
+
+awslocal dynamodb create-table --table-name test\
+ --attribute-definitions AttributeName=PK,AttributeType=S\
+  --key-schema AttributeName=PK,KeyType=HASH\
+   --billing-mode PAY_PER_REQUEST
